e196f20a4758712065a51a1209984ffde1f68809
Mohamed-sdet <ibrahim89.career@gmail.com>
2022-11-04 17:46:08 +0530

    Hash Map

--- Leetcodes/src/map/FindAllNumbersDisappearedinanArray.java
+++ Leetcodes/src/map/FindAllNumbersDisappearedinanArray.java
@@ -1,5 +1,10 @@
 package map;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Test;
+
 public class FindAllNumbersDisappearedinanArray {
 	
 	/*Solve using Hashing 
@@ -10,7 +15,53 @@ public class FindAllNumbersDisappearedinanArray {
 	 * 
 	 * 
 	 */
+//	@Test
+	public void examplePositiveData()
+	{
+		
+		int[]  nums = {4,3,2,7,8,2,3,1};
+		findAllNumbersDisappearedinanArray(nums);
+		
+	}
 	
+	@Test
+	public void exampleEdgeData()
+	{
+		
+		int[]  nums = {1,1};
+		findAllNumbersDisappearedinanArray(nums);
+		
+	}
+
+	private void findAllNumbersDisappearedinanArray(int[] nums) {
+		// TODO Auto-generated method stub
+		
+		int [] output =new int [2];
+		int counter =0;
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			set.add(nums[i]);
+
+			
+		}
+		System.out.print(set);
+		for(int j=1;j<=nums.length;j++)
+		{
+			
+		    if(!set.contains(j))
+		    {
+		    	output[counter]=j;
+		    	counter++;
+		    }
+			
+		}
+		
+		System.out.print(Arrays.toString(output));
+	}
 	
+
 
 }
